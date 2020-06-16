08a382a5c8a8d69681625b658c54d6128c86317e
mohd.afzal <mohd.afzal@tothenew.com>
2020-06-17 00:29:32 +0530

    Added slider

--- src/components/header/nav.js
+++ src/components/header/nav.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import './header.css';
+import './nav.scss';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -30,15 +30,18 @@ class Navigation extends Component {
     render() {
         return (
             <Navbar expand="lg" fixed="top" className={`${this.state.activeClass}`}>
-            <Navbar.Brand href="#home">WebServices</Navbar.Brand>
+            <Navbar.Brand href="/">
+              <span className="logo">AT</span>
+              <span className="text">tech</span>
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
                 <Nav.Link href="#home">HOME</Nav.Link>
                 <Nav.Link href="#link">ABOUT US</Nav.Link>
                 <Nav.Link href="#services">SERVICES</Nav.Link>
+                <Button variant="outline-success ml-2">CONTACT US</Button>
                 <Form inline>
-                    <Button variant="outline-success">Contact us</Button>
                 </Form>
                 </Nav>
             </Navbar.Collapse>
--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -4,58 +4,39 @@ import Col from 'react-bootstrap/Col';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import './slider.css';
+import './slider.scss';
+
 const SliderComponent = () => {
     const settings = {
         arrows: false,
-        adaptiveHeight: false,
+        adaptiveHeight: true,
         dots: true,
         autoplay: true,
-        autoplaySpeed: 4000,
+        autoplaySpeed: 5000,
         infinite: true,
-        speed: 500,
+        speed: 1500,
         slidesToShow: 1,
         slidesToScroll: 1,
         pauseOnHover: false,
         pauseOnDotsHover: true,
         pauseOnFocus: true,
-        fade: false,
+        fade: true,
       };    
     return (
         <Row>
             <Col md="12" className="p-0">
                 <Slider {...settings}>
-                    <div className="d-flex carousel">
-                        <div className="carousel-img-wrapper">
-                            <img className="carousel-img" src="https://static1.tothenew.com/sites/default/files/styles/banner_image_inner_large/public/2020-04/header_banner_bg.png?itok=h5Owzsgr" />
-                        </div>
-                        <div className="carousel-content">
-                            <h2>This is Screen 1</h2>
-                        </div>
+                    <div id="slide1" className="carousel-item">
+
                     </div>
-                    <div className="d-flex carousel">
-                        <div className="carousel-img-wrapper">
-                            <img className="carousel-img" src="https://static1.tothenew.com/sites/default/files/styles/banner_image_inner_large/public/2016-07/Success-Stories.jpg?itok=-cwgxevq" />
-                        </div>
-                        <div className="carousel-content">
-                            <h2>This is Screen 2</h2>
-                        </div>
+                    <div id="slide2" className="carousel-item">
+
                     </div>
-                    <div className="d-flex carousel">
-                        <div className="carousel-img-wrapper">
-                            <img className="carousel-img" src="https://static1.tothenew.com/sites/default/files/styles/banner_image_inner_large/public/2019-04/ecom-banner.png?itok=KUKM6bLi" />
-                        </div>
-                        <div className="carousel-content">
-                            <h2>This is Screen 3</h2>
-                        </div>
+                    <div id="slide3" className="carousel-item">
+
                     </div>
-                    <div className="d-flex carousel">
-                        <div className="carousel-img-wrapper">
-                            <img className="carousel-img" src="https://static1.tothenew.com/sites/default/files/styles/banner_image_inner_large/public/2016-07/Technology_0.jpg?itok=ndptHTsi" />
-                        </div>
-                        <div className="carousel-content">
-                            <h2>This is Screen 4</h2>
-                        </div>
+                    <div id="slide4" className="carousel-item">
+
                     </div>
                 </Slider>
             </Col>
@@ -64,3 +45,36 @@ const SliderComponent = () => {
 
 }
 export default SliderComponent;
+
+{/* <div className="d-flex carousel">
+    <div className="carousel-img-wrapper">
+        <img className="carousel-img" src={require('../../images/green-potted-plant-on-white-ceramic-vase-1166644.jpg')} />
+    </div>
+    <div className="carousel-content">
+        <h2>This is Screen 1</h2>
+    </div>
+</div>
+<div className="d-flex carousel">
+    <div className="carousel-img-wrapper">
+        <img className="carousel-img" src="https://static1.tothenew.com/sites/default/files/styles/banner_image_inner_large/public/2016-07/Success-Stories.jpg?itok=-cwgxevq" />
+    </div>
+    <div className="carousel-content">
+        <h2>This is Screen 2</h2>
+    </div>
+</div>
+<div className="d-flex carousel">
+    <div className="carousel-img-wrapper">
+        <img className="carousel-img" src="https://static1.tothenew.com/sites/default/files/styles/banner_image_inner_large/public/2019-04/ecom-banner.png?itok=KUKM6bLi" />
+    </div>
+    <div className="carousel-content">
+        <h2>This is Screen 3</h2>
+    </div>
+</div>
+<div className="d-flex carousel">
+    <div className="carousel-img-wrapper">
+        <img className="carousel-img" src="https://static1.tothenew.com/sites/default/files/styles/banner_image_inner_large/public/2016-07/Technology_0.jpg?itok=ndptHTsi" />
+    </div>
+    <div className="carousel-content">
+        <h2>This is Screen 4</h2>
+    </div>
+</div> */}
