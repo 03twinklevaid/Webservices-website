0b01a597b4bbdcd673420d3f5087ffbd46bc894d
Twinkle vaid <twinkle@tothenew.com>
2020-06-18 17:52:08 +0530

    Changed the alignment of about text

--- src/components/aboutus/aboutus.js
+++ src/components/aboutus/aboutus.js
@@ -13,7 +13,7 @@ const About = () => {
                     <h1>Overview</h1>
                     <hr />
                 </Col>
-                <Col md="12" className="about-us__text">
+                <Col md="6" className="about-us__text">
                     <p>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem
                     </p>
