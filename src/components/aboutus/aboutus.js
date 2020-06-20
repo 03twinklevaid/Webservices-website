49676bacb4316c06af3b9e1527b14640091568f4
Twinkle vaid <twinkle@tothenew.com>
2020-06-20 23:28:42 +0530

    AOS animations and some images added

--- src/components/aboutus/aboutus.js
+++ src/components/aboutus/aboutus.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './aboutus.scss';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Aos from 'aos';
 
-const About = () => {
-    return(
-        <div className="about-us">
-            <Container>
-              <Row>
-                <Col md="12" className="text-left">
-                    <h1>Overview</h1>
-                    <hr />
-                </Col>
-                <div>
-                <Col md="7" className="about-us__text">
-                    <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem.
-                    </p>
-                </Col>
-                    
-                </div>
-              </Row>
-            </Container> 
-        </div>
-    )
+class About extends Component {
+    constructor() {
+        super();
+    }
+    componentWillReceiveProps () {
+        Aos.refresh();
+    }
+    render() {
+        return(
+            <div className="about-us">
+                <Container>
+                <Row>
+                    <Col md="12" className="text-left" data-aos="fade-up">
+                        <h1>Overview</h1>
+                        <hr />
+                    </Col>
+                    <div>
+                    <Col md="7" className="about-us__text" data-aos="fade-up">
+                        <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem.
+                        </p>
+                    </Col>
+                        
+                    </div>
+                </Row>
+                </Container> 
+            </div>
+        )
+    }
 }
 
 export default About;
